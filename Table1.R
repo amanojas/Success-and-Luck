93d74d005b66035b56c04c9e3e3da0826636cbdb
Aman Desai <44756161+amanojas@users.noreply.github.com>
2019-10-19 00:52:26 -0400

    Update Table1.R

--- Table1.R
+++ Table1.R
@@ -1,3 +1,4 @@
+library(repmis)
 library(knitr)
 ques2 = repmis::source_data("https://raw.githubusercontent.com/amanojas/Success-and-Luck/master/Datatable1.csv")
 ques2 = matrix(data = ques2$V1 , nrow = 3, ncol = 3, byrow = TRUE,dimnames = list(c("Smoker","Non-smoker","Total"),c("Male","Female","Total")))
